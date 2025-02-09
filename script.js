0f107df21eb2493bd0b8e9da656bf5c50d3ebc1f
Yakov Bronnikov <52982731+yakovbronnikov@users.noreply.github.com>
2025-02-10 00:18:20 +1000

    fix: service worker

--- script.js
+++ script.js
@@ -272,3 +272,14 @@ function saveImage(e) {
 
 
 clearCanvas()
+
+
+// SW
+
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('./sw.js')
+      .then(() => navigator.serviceWorker.ready.then((worker) => {
+        worker.sync.register('syncdata');
+      }))
+      .catch((err) => console.log(err));
+}
